2ba5d762153f6996d207b8174671d7220cb4c244
Oskar Hahn <mail@oshahn.de>
2024-05-28 14:56:09 +0200

    Use a Task in the example app

--- roc_app_templates/app.h
+++ roc_app_templates/app.h
@@ -1,3 +1,24 @@
 #include "roc_std.h"
 
-extern void roc__mainForHost_1_exposed_generic(const struct RocStr *data);
+union ResultVoidI32Union {
+    long int exit_code;
+};
+
+struct ResultVoidI32 {
+    union ResultVoidI32Union payload;
+    unsigned char disciminant;
+};
+
+
+union ResultVoidStrUnion {
+    struct RocStr str;
+};
+
+struct ResultVoidStr {
+    union ResultVoidStrUnion payload;
+    unsigned char disciminant;
+};
+
+void roc__mainForHost_1_exposed_generic(void* captures);
+size_t roc__mainForHost_1_exposed_size();
+void roc__mainForHost_0_caller(char* flags, void* closure_data, struct ResultVoidI32 *result);
